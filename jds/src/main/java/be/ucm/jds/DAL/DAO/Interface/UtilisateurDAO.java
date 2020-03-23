3fd34d8c3b1f1fefffad51bc682f2871c7b86a5e
Bryan <bryanlockkamper2.gmail.com>
2020-03-23 15:27:41 +0100

    update pseudo correction

--- jds/src/main/java/be/ucm/jds/DAL/DAO/Interface/UtilisateurDAO.java
+++ jds/src/main/java/be/ucm/jds/DAL/DAO/Interface/UtilisateurDAO.java
@@ -4,12 +4,15 @@ import be.ucm.jds.DAL.Entity.JeuDAL;
 import be.ucm.jds.DAL.Entity.RencontreDAL;
 import be.ucm.jds.DAL.Entity.UtilisateurDAL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 @Repository
 public interface UtilisateurDAO extends JpaRepository<UtilisateurDAL, Long> {
 
@@ -45,6 +48,7 @@ public interface UtilisateurDAO extends JpaRepository<UtilisateurDAL, Long> {
 
     //mise a jour
 
+    @Modifying
     @Query("update Utilisateur set pseudo = :pseudo WHERE id = :id")
     void savePseudo(String pseudo, Long id);
 }
